dde253c9c61bd0acc353ff923adecd5afac058a1
fabiopievani <48036927+fabiopievani@users.noreply.github.com>
2025-05-16 19:37:38 +0200

    Create ascii_visualization.py

---
+++ ascii_visualization.py
@@ -0,0 +1,36 @@
+# Constants used when drawing gates
+PLUS2_ASCII =       ["  +----+  ",
+                     "--| +2 |--",
+                     "  +----+  "]
+PLUS1_ASCII =       ["  +----+  ",
+                     "--| +1 |--",
+                     "  +----+  "]
+ID_ASCII =          ["  +----+  ",
+                     "--| ID |--",
+                     "  +----+  "]
+ONE_TWO_ASCII =     ["  +----+  ",
+                     "--| 12 |--",
+                     "  +----+  "]
+ZERO_TWO_ASCII =    ["  +----+  ",
+                     "--| 02 |--",
+                     "  +----+  "]
+ZERO_ONE_ASCII =    ["  +----+  ",
+                     "--| 01 |--",
+                     "  +----+  "]
+CONTROL_ASCII =     ["          ",
+                     "----O-----",
+                     "    |     "]
+CONTROL_ASCII_REV = ["    |     ",
+                     "----O-----",
+                     "          "]
+WIRE_ASCII =        ["          ",
+                     "----------",
+                     "          "]
+LINE_ASCII =        ["    |     ",
+                     "----|-----", 
+                     "    |     "]
+BARRIER_ASCII =     ["    ||    ",
+                     "----||----", 
+                     "    ||    "]
+def custom_ascii(name): # Function that generates the ascii representation of a custom gate 
+    return ["  +----+  ",f"--|{name:^4}|--","  +----+  "]
