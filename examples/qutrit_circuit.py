848415dfe3eccb5972da168da67518bb984d8186
fabiopievani <48036927+fabiopievani@users.noreply.github.com>
2025-05-17 00:53:10 +0200

    Update qutrit_circuit.py

--- examples/qutrit_circuit.py
+++ examples/qutrit_circuit.py
@@ -4,18 +4,6 @@ example = QuditCircuit(num_qudit=3)
 
 example.plus1(target=1)
 
-example.draw()
-
-#OUTPUT:      
-              
-#  Q0   ---------------
-#                      
-#             +-----+  
-#  Q1   ------| + 1 |--
-#             +-----+  
-#                      
-#  Q2   ---------------
-
 example.c_zero_one(control=0,target=1)
 
 example.draw()
